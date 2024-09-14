42ee88362c0b8c8051146de826faa6d81e60233f
shengtsui <sheng@vorticity.xyz>
2024-09-13 17:50:31 -0700

    doc

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import pathlib
+import sys
+import warnings
+from sphinx.deprecation import RemovedInSphinx90Warning
+
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+warnings.filterwarnings("ignore", category=RemovedInSphinx90Warning)
+project = 'VTensor'
+copyright = '2024, Sheng-Yang Tsui'
+author = 'Sheng-Yang Tsui'
+release = 'v1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.doctest',
+              'sphinx.ext.extlinks',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon',
+              'sphinx_copybutton']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+pygments_style = 'sphinx'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,31 @@
+VTensor documentation
+=====================
+
+VTensor, a C++ library, facilitates tensor manipulation on GPUs, emulating the python-numpy style for ease of use. 
+It leverages RMM (RAPIDS Memory Manager) for efficient device memory management. 
+The library integrates CuRand, CuBLAS, and CuSolver to support a wide range of operations, including mathematics, linear algebra, and random number generation.
+Explore one of our examples for a quick overview!
+
+.. code-block:: cpp
+
+   #include <vtensor/lib/vtensor.hpp>
+
+   int main() {
+      auto num_points = 100000;
+      auto points = vt::random::rand(2, num_points); // 2D random points
+      auto dist = vt::sqrt(vt::sum(vt::power(points, 2.0f), 0)); // Euclidean distance along the axis 0
+      auto inside_points = vt::sum(dist < 1.0f); // Count points inside the unit circle
+      auto pi_estimate = double(inside_points) / num_points * 4; // Estimate pi
+   }
+
+
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. toctree::
+
+   installation
+   tutorials/index
+   api/index
